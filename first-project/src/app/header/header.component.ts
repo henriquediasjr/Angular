9aaa76c0f30fa41809b99e5f65d0d45b10f85d8a
Henrique Dias <henriquediasjr@gmail.com>
2021-04-20 20:21:53 -0300

    learning it

--- first-project/src/app/header/header.component.ts
+++ first-project/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+
 
 @Component({
     selector:'app-header',
@@ -8,4 +9,9 @@ import { Component } from "@angular/core";
 
 export class HeaderComponent{
     collapsed = true;
+    @Output()  featureSelected = new EventEmitter<string>()
+    
+    onSelect(feature: string){
+        this.featureSelected.emit(feature)
+    }
 }
