edb1d0250d4e8cb94394127124c420e4200b4d63
Henrique Dias <henriquediasjr@gmail.com>
2021-04-28 15:07:01 -0300

    /

--- directive-start/src/app/app.module.ts
+++ directive-start/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
