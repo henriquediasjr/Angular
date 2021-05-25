28c891fc5062fc8fcbfdc1d1f05e87f8e9174682
Henrique Dias <henriquediasjr@gmail.com>
2021-05-25 15:25:47 -0300

    changing

--- services-start/src/app/account/account/account.component.ts
+++ services-start/src/app/account/account/account.component.ts
@@ -1,18 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent {
-  @Input() account: {name: string, status: string};
-  @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+export class AccountComponent implements OnInit {
 
+  constructor() { }
 
-  onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+  ngOnInit(): void {
   }
+
 }
