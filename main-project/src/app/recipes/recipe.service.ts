a3f9d81351da4f6a26433bfc5b4acf1a887d3a31
Henrique Dias <henriquediasjr@gmail.com>
2021-06-16 17:30:29 -0300

    main section services

--- main-project/src/app/recipes/recipe.service.ts
+++ main-project/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 import { Recipe } from "./recipe.model";
 
@@ -6,8 +7,8 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ119Dd7KPW6akHWrSnKqfW_kbihJeb1P7DNw&usqp=CAU'),
-        new Recipe('Second Version', 'This is the 2.0 test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ119Dd7KPW6akHWrSnKqfW_kbihJeb1P7DNw&usqp=CAU')
+        new Recipe('Carne moida com Brocolis', 'Receita simples com muito tempeiro', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ119Dd7KPW6akHWrSnKqfW_kbihJeb1P7DNw&usqp=CAU', [ new Ingredient('Carne moida', 1), new Ingredient('brócolis', 8) ]),
+        new Recipe('Açaí com Leite ninho', 'Tigela de açaí tropical', 'https://media-cdn.tripadvisor.com/media/photo-s/07/17/a5/75/acai-com-leite-ninho.jpg', [ new Ingredient('açaí', 1), new Ingredient('leite ninho', 5) ])
       ];
 
     getRecipes() {
