1095a860b911966132988811eb430a6b703665c7
Henrique Dias <henriquediasjr@gmail.com>
2021-08-11 18:16:52 -0300

    routing chapter

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -8,14 +9,19 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 }) 
 export class EditServerComponent implements OnInit {
-  server!: { id: number; name: string; status: string; };
-  serverName = '';
-  serverStatus = '';
+  server!: { id: number, name: string, status: string }; //resolver problema de quando tira aquele ! toda a página quebra
+  serverName = "";
+  serverStatus = "";
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server != this.serversService.getServer(1);
+    console.log(this.route.snapshot.queryParams)
+    console.log(this.route.snapshot.fragment)
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
+    this.server != this.serversService.getServer(1); 
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
